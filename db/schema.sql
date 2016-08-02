43f0096d2d2bdadf1058e5dcf386a3ec964ea1e4
Todd Germaine <trgermaine@gmail.com>
2016-08-02 18:18:54 -0400

    updated schema

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+ id INT NOT NULL AUTO_INCREMENT,
+ burger_name: VARCHAR (50),
+ devoured: BOOLEAN NOT NULL,
+ date: TIMESTAMP NOT NULL,
+ PRIMARY KEY (id)
+);
---
+++ server.js
@@ -0,0 +1,5 @@
+//Dependencies
+
+var express = require ('./express');
+var method_override = require ('./method-override');
+var body_parser = require ('./body-parser');
