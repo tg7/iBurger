a494e1409e03b69d4e699f33df07c79db57b4c92
Todd Germaine <trgermaine@gmail.com>
2016-08-04 23:35:38 -0400

    updated files

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,10 @@ USE burgers_db;
 
 CREATE TABLE burgers (
  id INT NOT NULL AUTO_INCREMENT,
- burger_name: VARCHAR (50),
- devoured: BOOLEAN NOT NULL,
- date: TIMESTAMP NOT NULL,
+ burger_name VARCHAR (50),
+ devoured BOOLEAN NOT NULL,
+ date TIMESTAMP NOT NULL,
  PRIMARY KEY (id)
 );
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+/* Dummy Data */
+
+INSERT INTO burgers(id,burger_name,devoured,date)
+VALUES ("BLT", false),
+       ("Big Mac", false),
+       ("Quarter Pounder", false),
+       ("Buffalo Burger", false),
+       ("Veggie Burger", false);
+   
+
+
